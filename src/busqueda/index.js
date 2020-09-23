108ba97539d2cf084240c5d07eb5df93ad0e55c7
SJMC29 <70499799+SJMC29@users.noreply.github.com>
2020-09-23 08:58:56 -0500

    Miércoles 9 am.
    Algoritmo de ordenamiento.

--- src/busqueda/index.js
+++ src/busqueda/index.js
@@ -26,46 +26,86 @@ function solve(problem, root) {
     // START CODE HERE
     let tree = [{pos: root, cost: 0, parent: null, action: null}];
     
-    addNode(problem,tree,0);
+    addNode(problem,tree,tree.length-1);
+    console.log("Ordenar: " + ordenarArbol(tree));
 
     // END CODE HERE
-    return { solution, cost }
-    //return tree;
-    //hola como estas main vladimisc
-    //#comiendo_pan
+    //return { solution, cost }
+    return tree;
+}
+
+function recursiva(){
+
 }
 
 function addNode(problem,tree,posNode){
+    //aqui
+    //tree[posNode].expanded = true;
+
     //Extender arriba?
     if(tree[posNode].pos[0]-1 >= 0 && "D" != tree[posNode].action)
     {   
         let newPos = [tree[posNode].pos[0]-1,tree[posNode].pos[1]];
-        tree.push({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode].pos, action: "U"});
+        tree.unshift({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode], action: "U"});
+        posNode++;
     }
 
     //Extender derecha?
     if(tree[posNode].pos[1]+1 < problem.maze[0].length && "L" != tree[posNode].action)
     {   
         let newPos = [tree[posNode].pos[0],tree[posNode].pos[1]+1];
-        tree.push({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode].pos, action: "R"});
+        tree.unshift({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode], action: "R"});
+        posNode++;
     }
 
     //Extender abajo?
     if(tree[posNode].pos[0]+1 < problem.maze.length && "U" != tree[posNode].action)
     {   
         let newPos = [tree[posNode].pos[0]+1,tree[posNode].pos[1]];
-        tree.push({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode].pos, action: "D"});
+        tree.unshift({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode], action: "D"});
+        posNode++;
     }
 
     //Extender izquierda?
     if(tree[posNode].pos[1]-1 >= 0 && "R" != tree[posNode].action)
     {   
         let newPos = [tree[posNode].pos[0],tree[posNode].pos[1]-1];
-        tree.push({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode].pos, action: "L"});
+        tree.unshift({pos: newPos, cost: tree[posNode].cost + problem.maze[newPos[0]][newPos[1]], parent: tree[posNode], action: "L"});
+        posNode++;
     }
+
+    tree.splice(posNode,1)
 }
+
+function ordenarArbol(tree) {
+    const tamaño = tree.length;
+    for (let i = 0; i < tamaño; i++ ) 
+    {
+      for (let j = 0; j < tamaño - 1 - i; j++ ) 
+      {
+        if ( tree[j].cost > tree[j + 1].cost ) 
+        {
+          [ tree[j].cost, tree[j + 1].cost ] = [ tree[j + 1].cost, tree[j].cost ];
+        }
+      }
+    }
+    return tree;
+};
+
+/*function ordenarArbol(tree){
+    sortTree = tree[0];
+    for(var i = 1; i < tree.length; i++)
+    {
+        if (tree[i] < sortTree[])
+        {
+            sortTree.push(tree[i]);
+        }
+    }
+}*/
+
 
 test = [{sara : 1},{juan : 2}];
 test.push({pitochu : 1});
 
 console.log(solve(problem, start));
+
