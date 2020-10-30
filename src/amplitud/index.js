5832ef25dd039206fba559405685233bb95cf119
Git-Fanfo <51414591+GitFanfo@users.noreply.github.com>
2020-10-30 10:19:53 -0500

    ?

--- src/amplitud/index.js
+++ src/amplitud/index.js
@@ -23,7 +23,7 @@ async function processLineByLine() {
       try {
             console.log(colors.brightMagenta('Loading data...\n'));
             //Crear el maze
-            for(var i = 0;i < arrayInput.length && arrayInput[i].indexOf(",")==-1;i=0){
+            for(let i = 0;i < arrayInput.length && arrayInput[i].indexOf(",")==-1;i=0){
                 level.push(arrayOfArrays(i).map(
                     function(x) {
                         if(x==0){return parseInt(x, 10)}
@@ -33,7 +33,7 @@ async function processLineByLine() {
                 }
             
             //Crear las posiciones
-            for(var i=0;i<arrayInput.length;i++){
+            for(let i=0;i<arrayInput.length;i++){
                 if(arrayInput[i] != []){
                     positions.push(arrayOfArrays(i).map(function(x) {
                         return parseInt(x, 10);
@@ -43,7 +43,7 @@ async function processLineByLine() {
         
             function arrayOfArrays(i) {
                 place = [];
-                for(var j = 0;j<arrayInput[i].length;j++){
+                for(let j = 0;j<arrayInput[i].length;j++){
                     if(arrayInput[i][j]!=','){
                         place.push(arrayInput[i][j]);   
                     }
@@ -131,19 +131,56 @@ async function fetchingData() {
         let solution = [];
         let nodos = [];
         let nodoEvaluado = nodo;
+        let expandidos = [];
 
-        const limite = 3;
-
-        while (!testGoal(nodoEvaluado, problem) &&  nodoEvaluado.level < limite) {
+        const limite = 30;
+        //&&  nodoEvaluado.level <= limite
+        while (!testGoal(nodoEvaluado, problem)) {
+            console.log('las cajas: '+nodoEvaluado.pos_Box)
+            console.log(nodoEvaluado.pos_Box)
+            created.push(nodoEvaluado.pos_Box);
+            try {
+                while(deleteState(nodoEvaluado.pos_Box,maze)){
+                    //console.log('borrando: '+nodoEvaluado.pos_Box)
+                    nodoEvaluado = nodos[0];
+                    nodos.shift();
+                }
+            } catch (error) {
+                break;
+            }
+                                   
             agregarNodos(problem.maze,nodoEvaluado,nodos);
+            
             nodoEvaluado = nodos[0];
+            console.log('Evaluando: '+nodoEvaluado.action)
+            console.log(nodoEvaluado.level)
+            //console.log(nodoEvaluado.pos)
+            
+            expandidos.push([nodoEvaluado.level,nodoEvaluado.pos,nodoEvaluado.action])
+            
+            /*            
+            if(deleteState(nodoEvaluado.pos_Box,maze)){
+                console.log('The Lost');
+                nodoEvaluado = nodos[1];
+            }
+
+            console.log('Sensores del pana')
+            console.log('U: '+busy(nodoEvaluado.pos,nodoEvaluado.pos_Box,maze,'U'));
+            console.log('D: '+busy(nodoEvaluado.pos,nodoEvaluado.pos_Box,maze,'D'));
+            console.log('L: '+busy(nodoEvaluado.pos,nodoEvaluado.pos_Box,maze,'L'));            
+            console.log('R: '+busy(nodoEvaluado.pos,nodoEvaluado.pos_Box,maze,'R'));
+            */
+            
             nodos.shift();
-            //console.log(nodos[0]);
         }
 
-        //console.log(nodos);
-        //console.log('creados:'+created)
-        trazarRuta(nodoEvaluado,solution);
+        console.log(expandidos);
+        console.log(colors.brightMagenta('creados'))
+        console.log(created)
+        if(nodoEvaluado==null){
+            solution = []
+        }else trazarRuta(nodoEvaluado,solution);
+        
         return { solution }
     }
 
@@ -160,17 +197,39 @@ async function fetchingData() {
             let colum = padre.pos[1];
             let niveles = padre.level + 1;
             let cajas = padre.pos_Box;
+                if(box_Address(padre.pos,padre.pos_Box,1,'U')){
+                    let bool = false;
+                    for(let i=0;i<padre.pos_Box.length && !bool;i++){
+                        bool = fila==cajas[i][0] && colum==cajas[i][1];
+                        if(bool){
+                            cajas[i][0] = cajas[i][0]-1;
+                            cajas[i][1] = cajas[i][1];
+                            //console.log(cajas);
+                        }
+                    }
+                }
+                console.log(cajas);
             nodos.push(crearNodo([fila,colum], cajas, niveles, padre, "U"))
-            console.log('box in U: '+ box_Address(padre.pos,padre.pos_Box,1,'U'));
-            created.push('U');
         }
+
         if(avanzar(maze,padre,'D')){ 
             let fila = padre.pos[0]+1;
             let colum = padre.pos[1];
             let niveles = padre.level + 1;
             let cajas = padre.pos_Box;
-            created.push('D');
-            console.log('box in D: '+ box_Address(padre.pos,padre.pos_Box,1,'D'));
+                if(box_Address(padre.pos,padre.pos_Box,1,'D')){
+                    let bool = false;
+                    for(let i=0;i<padre.pos_Box.length && !bool;i++){
+                        bool = fila==padre.pos_Box[i][0] && colum==padre.pos_Box[i][1];
+                        if(bool){
+                            cajas[i][0] = cajas[i][0]+1;
+                            cajas[i][1] = cajas[i][1];
+                            //console.log(cajas);
+                        }
+                    }
+                }
+                console.log(cajas);              
+            //console.log('ABAJO');
             nodos.push(crearNodo([fila,colum],cajas, niveles, padre, "D"))
         }
         if(avanzar(maze,padre,'L')){
@@ -178,8 +237,18 @@ async function fetchingData() {
             let colum = padre.pos[1]-1;
             let niveles = padre.level + 1;
             let cajas = padre.pos_Box;
-            created.push('L');
-            console.log('box in L: '+ box_Address(padre.pos,padre.pos_Box,1,'L'));
+                if(box_Address(padre.pos,padre.pos_Box,1,'L')){
+                    let bool = false;
+                    for(let i=0;i<padre.pos_Box.length && !bool;i++){
+                        bool = fila==padre.pos_Box[i][0] && colum==padre.pos_Box[i][1];
+                        if(bool){
+                            cajas[i][0] = cajas[i][0];
+                            cajas[i][1] = cajas[i][1]-1;
+                        }                       
+                    }
+                }
+                console.log(cajas);
+            //console.log('IZQUIERDA');                
             nodos.push(crearNodo([fila,colum],cajas, niveles, padre, "L"))
         }
         //console.log('voy a la derecha: '+avanzar(maze,padre,'R'))
@@ -188,8 +257,19 @@ async function fetchingData() {
             let colum = padre.pos[1]+1;
             let niveles = padre.level + 1;
             let cajas = padre.pos_Box;
-            created.push('R');
-            console.log('box in R: '+ box_Address(padre.pos,padre.pos_Box,1,'R'));
+                if(box_Address(padre.pos,padre.pos_Box,1,'R')){
+                    let bool = false;
+                    for(let i=0;i<padre.pos_Box.length && !bool;i++){
+                        bool = fila==padre.pos_Box[i][0] && colum==padre.pos_Box[i][1];
+                        if(bool){
+                            cajas[i][0] = cajas[i][0];
+                            cajas[i][1] = cajas[i][1]+1;
+                            //console.log(cajas);
+                        }
+                    }
+                }
+                console.log(cajas);
+            //console.log('DERECHA');
             nodos.push(crearNodo([fila,colum],cajas, niveles, padre, "R"))
         }
 
@@ -209,45 +289,188 @@ async function fetchingData() {
 
     function avanzar(maze,padre,dir){
         let caja_caja = false;
+        let caja_pared = false;
+        let caja_lost = false;
+
+        let caja_arriba = [padre.pos[0]-2,padre.pos[1]];
+        let caja_abajo = [padre.pos[0]+2,padre.pos[1]];
+        let caja_izq = [padre.pos[0],padre.pos[1]-2];
+        let caja_der = [padre.pos[0],padre.pos[1]+2];
         switch (dir) {
             case 'U':
-                caja_caja =  box_Address(padre.pos,padre.pos_Box,1,'U') && box_Address(padre.pos,padre.pos_Box,2,'U')           
-                return padre.pos[0] > 0 && wall_Address(maze,padre,'U') && !caja_caja;
+                caja_caja = box_Address(padre.pos,padre.pos_Box,1,'U') && box_Address(padre.pos,padre.pos_Box,2,'U')
+                caja_pared = box_Address(padre.pos,padre.pos_Box,1,'U') && !wall_Address(maze,padre.pos,2,'U')
+
+                caja_lost = box_Address(padre.pos,padre.pos_Box,1,'U') && (busy(caja_arriba,padre.pos_Box,maze,'U') && (busy(caja_arriba,padre.pos_Box,maze,'R') || busy(caja_arriba,padre.pos_Box,maze,'L')));
+                    if(caja_lost){
+                        console.log(colors.brightGreen('U perdio'));
+                    }
+
+                return padre.pos[0] > 0 && wall_Address(maze,padre.pos,1,'U') && !caja_caja && !caja_pared;// && !caja_lost;
             case 'D':           
                 caja_caja =  box_Address(padre.pos,padre.pos_Box,1,'D') && box_Address(padre.pos,padre.pos_Box,2,'D')
-                return padre.pos[0] < maze.length - 1 && wall_Address(maze,padre,'D') && !caja_caja;
+                caja_pared = box_Address(padre.pos,padre.pos_Box,1,'D') && !wall_Address(maze,padre.pos,2,'D')
+                
+                caja_lost = box_Address(padre.pos,padre.pos_Box,1,'D') && (busy(caja_abajo,padre.pos_Box,maze,'D') && (busy(caja_abajo,padre.pos_Box,maze,'R') || busy(caja_abajo,padre.pos_Box,maze,'L')));
+                    if(caja_lost){
+                        console.log(colors.brightGreen('D perdio'));
+                    }
+                               
+                return padre.pos[0] < maze.length - 1 && wall_Address(maze,padre.pos,1,'D') && !caja_caja && !caja_pared;// && !caja_lost;
             case 'L':              
                 caja_caja =  box_Address(padre.pos,padre.pos_Box,1,'L') && box_Address(padre.pos,padre.pos_Box,2,'L')
-                return padre.pos[1] > 0 && wall_Address(maze,padre,'L') && !caja_caja;
+                caja_pared = box_Address(padre.pos,padre.pos_Box,1,'L') && !wall_Address(maze,padre.pos,2,'L')
+
+                caja_lost = box_Address(padre.pos,padre.pos_Box,1,'L') && (busy(caja_izq,padre.pos_Box,maze,'L') && (busy(caja_izq,padre.pos_Box,maze,'U') || busy(caja_izq,padre.pos_Box,maze,'D')));
+                    if(caja_lost){
+                        console.log(colors.brightGreen('L perdio'));
+                    }
+
+                return padre.pos[1] > 0 && wall_Address(maze,padre.pos,1,'L') && !caja_caja && !caja_pared;// && !caja_lost;
             case 'R':               
                 caja_caja =  box_Address(padre.pos,padre.pos_Box,1,'R') && box_Address(padre.pos,padre.pos_Box,2,'R')
-                return padre.pos[1] < maze[0].length && wall_Address(maze,padre,'R') && !caja_caja;
+                caja_pared = box_Address(padre.pos,padre.pos_Box,1,'R') && !wall_Address(maze,padre.pos,2,'R')
+
+                caja_lost = box_Address(padre.pos,padre.pos_Box,1,'R') && (busy(caja_der,padre.pos_Box,maze,'R') && (busy(caja_der,padre.pos_Box,maze,'U') || busy(caja_der,padre.pos_Box,maze,'D')));
+                    if(caja_lost){
+                        console.log(colors.brightGreen('R perdio'));
+                    }
+
+                return padre.pos[1] < maze[0].length && wall_Address(maze,padre.pos,1,'R') && !caja_caja && !caja_pared;// && !caja_lost;
           }
     }
 
-    function wall_Address(maze,padre,dir){
+    //true si el estado yaper
+    function deleteState(array,maze){
+        let bool = true;
+        let arrBool = [];
+
+        for(let i=0;i<array.length;i++){
+            arrBool.push(false);
+        }
+
+        for(let i=0;i<array.length;i++){
+            if(busy(array[i],array,maze,'U') && busy(array[i],array,maze,'R')){
+                //console.log('caja '+i+': arriba: '+busy(array[i],array,maze,'U'))
+                //console.log('caja '+i+': derecha: '+busy(array[i],array,maze,'D'))
+                arrBool[i] = true;
+            }
+
+            if(busy(array[i],array,maze,'R') && busy(array[i],array,maze,'D')){
+                //console.log('caja '+i+': derecha: '+busy(array[i],array,maze,'R'))
+                //console.log('caja '+i+': abajo: '+busy(array[i],array,maze,'D'))
+                arrBool[i] = true;
+            }
+
+            if(busy(array[i],array,maze,'D') && busy(array[i],array,maze,'L')){
+                //console.log('caja '+i+': abajo: '+busy(array[i],array,maze,'D'))
+                //console.log('caja '+i+': izquierda: '+busy(array[i],array,maze,'L'))
+                arrBool[i] = true;
+            }
+
+            if(busy(array[i],array,maze,'L') && busy(array[i],array,maze,'U')){
+                //console.log('caja '+i+': izquierda: '+busy(array[i],array,maze,'L'))
+                //console.log('caja '+i+': arriba: '+busy(array[i],array,maze,'U'))
+                arrBool[i] = true;
+            }
+        }
+
+        for(let i=0;i<array.length;i++){
+            bool = bool && arrBool[i];
+        }
+
+        return bool;
+    }
+
+    function busy(pos,array,maze,dir){
         switch (dir) {
             case 'U':                
                 try {
-                    return maze[padre.pos[0]-1][padre.pos[1]]!= 'W';
+                    return box_Address(pos,array,1,'U') || !wall_Address(maze,pos,1,'U')
+                } catch (error) {
+                    return false
+                }                
+            case 'D':                
+                try {
+                    return box_Address(pos,array,1,'D') || !wall_Address(maze,pos,1,'D')
+                } catch (error) {
+                    return false
+                }
+
+            case 'L':                
+                try {
+                    //console.log('busy L 1: '+ box_Address(pos,array,1,'L'))
+                    //console.log('busy L 2: '+ wall_Address(maze,pos,1,'L'))
+                    return box_Address(pos,array,1,'L') || !wall_Address(maze,pos,1,'L')
+                } catch (error) {
+                    return false
+                }
+
+            case 'R':                
+                try {
+                    return box_Address(pos,array,1,'R') || !wall_Address(maze,pos,1,'R')
+                } catch (error) {
+                    return false
+                }
+        }
+
+    }
+
+    function limits_maze(padre,maze,dir){
+        switch (dir) {
+            case 'U':                
+                try {
+                    return padre.pos[0] > 0;
+                } catch (error) {
+                    return false
+                }
+                
+            case 'D':                
+            try {
+                return padre.pos[0] < maze.length - 1;
+            } catch (error) {
+                return false
+            }
+
+            case 'L':                
+            try {
+                return padre.pos[1] > 0;
+            } catch (error) {
+                return false
+            }
+
+            case 'U':                
+            try {
+                return padre.pos[1] < maze[0].length;
+            } catch (error) {
+                return false
+            }
+        }
+    }
+
+    function wall_Address(maze,pos,dist,dir){
+        switch (dir) {
+            case 'U':                
+                try {
+                    return maze[pos[0]-dist][pos[1]]!= 'W';
                 } catch (error) {
                     return false
                 }          
             case 'D': 
                 try {
-                    return maze[padre.pos[0]+1][padre.pos[1]]!= 'W';
+                    return maze[pos[0]+dist][pos[1]]!= 'W';
                 } catch (error) {
                    return false
                 }                           
             case 'L':                
                 try {
-                    return maze[padre.pos[0]][padre.pos[1]-1]!= 'W';
+                    return maze[pos[0]][pos[1]-dist]!= 'W';
                 } catch (error) {
                     return false
                 }    
             case 'R':  
                 try {
-                   return maze[padre.pos[0]][padre.pos[1]+1]!= 'W';
+                   return maze[pos[0]][pos[1]+dist]!= 'W';
                 } catch (error) {
                    return false
                 }      
@@ -262,7 +485,7 @@ async function fetchingData() {
             }
 
             if(dir == 'D'){                
-                bool = (pos[0]+dist==array[i][0] && pos[1]==array[i][1]);
+                bool = pos[0]+dist==array[i][0] && pos[1]==array[i][1];
             }
 
             if(dir == 'L'){
